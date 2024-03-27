5fad406e9dd883035f31de24034f315787e06278
Amnoshield <153954306+Amnoshield@users.noreply.github.com>
2024-03-27 18:37:20 -0400

    updated notes

--- notes.py
+++ notes.py
@@ -1,12 +1,10 @@
+""" find a way to use the GPU for the image prosessing. """
+
 """  add a way to select the number of bits """
 
 """ add restore / clear functions that only effect the selected bits. """
 
-""" add sort options for all options. """
-
-def sort_list(unsorted:list[str], idx:int, reverse:float=False):
-	key = lambda a:a[idx]
-	unsorted.sort(key=key, reverse=reverse)
+""" use a changed / encrypted password for the random.seed() """
 
 """ add password hide button???? """
 
