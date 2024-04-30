78d6f53a3fcedf3616f38438734e8730430360cb
Amnoshield <153954306+Amnoshield@users.noreply.github.com>
2024-04-30 11:05:34 -0400

    added my func and updated the timer to count itirations

--- old vs numpy image editing test.py
+++ old vs numpy image editing test.py
@@ -1,5 +1,7 @@
 from copy import deepcopy
+import random
 import time
+from typing import Generator
 
 import numpy as np
 from PIL import Image
@@ -14,6 +16,7 @@ def timer(itirations:int = 1, timeLimit:int = None):
 					break
 				start = time.time()
 				result = func(*deepcopy(args), **deepcopy(kwargs))
+				print(f'itiration count: {x+1}')
 				end = time.time()
 				numbers.append(end-start)
 			
@@ -34,6 +37,7 @@ def swap_bits(num:int, bit_index:int, bit_value:int|str) -> int:
 	new_num[-(bit_index+1)] = str(bit_value)
 	return int(''.join(new_num), 2)
 
+#numpy func
 #@timer(100, 30)
 def numpy_func(image:Image.Image, custom_function):
 	# Get the dimensions of the image
@@ -48,6 +52,73 @@ def numpy_func(image:Image.Image, custom_function):
 	print('starting to run numpy func now')
 	all_rgba_values = func(all_rgba_values)
 
+#my func
+def create_4d_array4(width:int, hight:int, key:str, colors:int = 4, num_bits = 3) -> Generator[list[list[list[tuple[int, int, int, int]]]], None, None]:
+	random.seed(key)
+	xList = list(range(width))
+	random.shuffle(xList)
+	yList = list(range(hight))
+	random.shuffle(yList)
+
+	counter = 0
+
+	y=yList[counter]
+	yield [[[(x, y, z, w) for w in range(num_bits)] for z in range(colors)] for x in range(width)]
+	#print('swap')
+	while True:
+		#print('X avoiding', yList[:counter+1])
+
+		x=xList[counter]
+		yield [[[(x, y, z, w) for w in range(num_bits)] for z in range(colors)] for y in range(hight) if y not in yList[:counter+1]]
+
+		counter += 1
+		if counter >= min([width, hight]): break
+
+		#print('swap')
+
+		#print('Y avoiding', xList[:counter])
+
+		y=yList[counter]
+		yield [[[(x, y, z, w) for w in range(num_bits)] for z in range(colors)] for x in range(width) if x not in xList[:counter]]
+		
+		#print('swap')
+
+	#input(f'{counter}, {[width, hight]}, {min([width, hight])}')
+	
+def create_Order3(width:int, hight:int, key:str, colors:int = 4, bits:int = 3)-> Generator[tuple[int, int, int, int], None, None]:
+	array: Generator[list[list[list[tuple[int, int, int, int]]]], None, None] = create_4d_array4(width, hight, key, colors)
+	random.seed(key)
+
+	for y in array:
+		random.shuffle(y)
+		for z in y:
+			random.shuffle(z)
+			for w in z:
+				random.shuffle(w)
+	
+		while len(y):
+			randY = random.randint(0, len(y)-1)
+			randZ = random.randint(0, len(y[randY])-1)
+			
+			yield from y[randY][randZ]
+			
+			y[randY].pop(randZ)
+			if not len(y[randY]):
+				y.pop(randY)
+
+@timer(100, 30)
+def mine(image:Image.Image, func):
+	pixel_data = image.load()
+	width, height = image.size
+	order = create_Order3(width, height, 'nerd', bits=width*height)
+	for _ in range(width*height):
+		x, y, z, w = next(order)
+		color = list(pixel_data[x, y])
+		color[z] = func(color[z])
+
+		pixel_data[x, y] = tuple(color)
+
+#func to use
 def func_to_use(value):
 	for x in range(4):
 		value = swap_bits(value, x, secrets.randbits(1))
@@ -61,4 +132,7 @@ def func_to_use(value):
 image = Image.open(input('Please enter image path: '))
 image = image.convert('RGBA')
 
+print('starting ')
+mine(image, func_to_use)
+print('------------')
 numpy_func(image, func_to_use)
