9490cebee44c194bdd2bbdfd878a4d976c668fd6
Amnoshield <153954306+Amnoshield@users.noreply.github.com>
2024-05-14 14:34:05 -0400

    updated notes

--- notes.py
+++ notes.py
@@ -1,7 +1,9 @@
-""" use np.random not random """
+""" use np.random not random???? """
 
 """ add password hide button???? """
 
 """ use random.getrandbits(8)???? """
 
 """ improve menu: don't show password, make a popup edit / see password menu with the password hidden and not editable until an edit button is shown. There should also be a bigger description field."""
+
+""" Improve settings / add more """
