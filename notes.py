abf108b7c2e4e3869d55379ded153557c88a9768
Amnoshield <153954306+Amnoshield@users.noreply.github.com>
2024-05-19 15:33:27 -0400

    updated notes

--- notes.py
+++ notes.py
@@ -1,9 +1,5 @@
-""" use np.random not random???? """
-
 """ Improve settings / add more """
 
 """ Add key confirmation for saving """
 
 """ add convert image function (like jpeg to png) """
-
-""" test np array for randomization? """
