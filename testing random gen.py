76915c13dbe5521557502fa5a996a549bbf0d241
Amnoshield <153954306+Amnoshield@users.noreply.github.com>
2024-05-23 12:59:28 -0400

    update to random testing

--- testing random gen.py
+++ testing random gen.py
@@ -63,7 +63,48 @@ def option1(width, height, key, num_bits):
 			random.shuffle(bits)
 		num += direction
 
-gen = option1(4000, 4000, 'testing', 3)
+def option2(width:int, height:int, key, num_bits:int, colors:int = 4):
+	random.seed(key)
+
+	nums = list(range(colors))
+	random.shuffle(nums)
+	direction = random.choice([-1, 1])
+	bits = list(range(num_bits))
+	for _ in range(colors):
+		num = nums.pop(0)
+		x_list = []
+		y_list = []
+		x_mask = list(range(width))
+		y_mask = list(range(height))
+		
+		while x_mask and y_mask:
+			if x_mask:
+				rand = random.choice(x_mask)
+				x_mask.remove(rand)
+				x_list.append(rand)
+				for y in y_list:
+					for bit in bits:
+						yield rand, y, (rand+y+num)%colors, bit
+			random.shuffle(x_list)
+			random.shuffle(bits)
+
+			if y_mask:
+				rand = random.choice(y_mask)
+				y_mask.remove(rand)
+				y_list.append(rand)
+				for x in x_list:
+					for bit in bits:
+						yield x, rand, (x+rand+num)%colors, bit
+			random.shuffle(y_list)
+			random.shuffle(bits)
+			
+""" gen = option1(4000, 4000, 'testing', 3)
 for x in gen:
 	thingy = [x, next(gen), next(gen)]
-	input(thingy)
+	input(thingy) """
+
+thingy = option2(4, 4, 'testing', 4)
+for x in thingy:
+	print([x]+[next(thingy) for _ in range(4)])
+
+quit()
