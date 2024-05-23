00f0dc257201197b0c3fbbe23a8e2dd21c5b90e3
Amnoshield <153954306+Amnoshield@users.noreply.github.com>
2024-05-23 13:02:01 -0400

    updated notes

--- notes.py
+++ notes.py
@@ -1,1 +1,6 @@
+
+""" Add new randomization (Option 2) Make sure to add in a correct key generation """
+
+""" I think there was something else but I don't remember """
+
 """ Make installer / compile """
--- testing random gen.py
+++ testing random gen.py
@@ -1,47 +1,4 @@
-import time
 import random
-from pprint import pprint
-
-def _timer(iterations:int = 1, timeLimit:int = 0):
-	import time
-	def decorator(func):
-		def inner(*args, **kwargs):
-			numbers = []
-			print(f"""----------
-Timing function: {func.__name__}
-----------""")
-			for x in range(iterations):
-				if timeLimit and sum(numbers) > timeLimit:
-					break
-				start = time.time()
-				result = func(*deepcopy(args), **deepcopy(kwargs))
-				print(f'iteration count: {x+1}')
-				end = time.time()
-				numbers.append(end-start)
-
-			print(f"""----------
-Timing done on function: {func.__name__}""")
-
-			avg = sum(numbers)/len(numbers)
-			_max = max(numbers)
-			_min = min(numbers)
-			if avg > 60:
-				avg /= 60
-				_max /= 60
-				_min /= 60
-				print(f'avg min: {avg}, max: {_max}, min: {_min}')
-			else:
-				print(f'avg sec: {avg}, max: {_max}, min: {_min}')
-
-			if len(numbers) != iterations:
-				print(f'Timer ended early with {len(numbers)} test(s) run instead of {iterations}')
-
-			print('----------')
-
-
-			return result
-		return inner
-	return decorator
 
 def option1(width, height, key, num_bits):
 	#random.seed(key)
